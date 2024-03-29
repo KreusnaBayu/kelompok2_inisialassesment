f18905d36d8d2b56a088b1221ab613d3aa9b460f
Rian Alfazri Khoirulloh <rian.alfazri96@gmail.com>
2024-03-29 22:44:21 +0700

    add button

--- lib/main.dart
+++ lib/main.dart
@@ -87,182 +87,211 @@ class _homePageState extends State<homePage> {
                   ),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Container(
-                  decoration: const BoxDecoration(
-                    color: Color.fromRGBO(66, 168, 195, 1),//rgba(66, 168, 195, 1)
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(20),
-                      bottomRight: Radius.circular(20),
+              Stack(
+                children: [
+                  SizedBox(
+                    height: 200,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          color: Color.fromRGBO(66, 168, 195, 1),//rgba(66, 168, 195, 1)
+                          borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(20),
+                            bottomRight: Radius.circular(20),
+                          ),
+                        ),
+                        child: const Padding(
+                          padding: EdgeInsets.only(top: 10, bottom: 30),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 40),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Expanded(
+                                      child: Text(
+                                        "Fajr",
+                                        textAlign: TextAlign.start,
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: Text(
+                                        "6:14",
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: Text(
+                                        "6:35",
+                                        textAlign: TextAlign.end,
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              // 2
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 40),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Expanded(
+                                      flex: 1,
+                                      child: Text(
+                                        "Dhuhar",
+                                        textAlign: TextAlign.start,
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ),
+                                    Expanded(
+                                      flex: 2,
+                                      child: Text(
+                                        "6:14",
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ),
+                                    Expanded(
+                                      flex: 1,
+                                      child: Text(
+                                        "6:35",
+                                        textAlign: TextAlign.end,
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              // 3
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 40),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Expanded(
+                                      child: Text(
+                                        "Asr",
+                                        textAlign: TextAlign.start,
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: Text(
+                                        "6:14",
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: Text(
+                                        "6:35",
+                                        textAlign: TextAlign.end,
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              // 4
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 40),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Expanded(
+                                      child: Text(
+                                        "Magrib",
+                                        textAlign: TextAlign.start,
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: Text(
+                                        "6:14",
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: Text(
+                                        "6:35",
+                                        textAlign: TextAlign.end,
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              // 5
+                              Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 40),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Expanded(
+                                      child: Text(
+                                        "Isha",
+                                        textAlign: TextAlign.start,
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: Text(
+                                        "6:14",
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ),
+                                    Expanded(
+                                      child: Text(
+                                        "6:35",
+                                        textAlign: TextAlign.end,
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                  child: const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 10.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 40),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Expanded(
-                                child: Text(
-                                  "Fajr",
-                                  textAlign: TextAlign.start,
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                              Expanded(
-                                child: Text(
-                                  "6:14",
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                              Expanded(
-                                child: Text(
-                                  "6:35",
-                                  textAlign: TextAlign.end,
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                            ],
+                  const SizedBox(height: 220,),
+                  Positioned(
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 40),
+                      child: ElevatedButton.icon(
+                        onPressed: () {
+                          
+                          print('Button pressed');
+                        },
+                        icon: Image.asset('assets/images/lokasi.png'), 
+                        label: const Text('Direction'),
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(13),
                           ),
                         ),
-                        // 2
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 40),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Expanded(
-                                flex: 1,
-                                child: Text(
-                                  "Dhuhar",
-                                  textAlign: TextAlign.start,
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                              Expanded(
-                                flex: 2,
-                                child: Text(
-                                  "6:14",
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                              Expanded(
-                                flex: 1,
-                                child: Text(
-                                  "6:35",
-                                  textAlign: TextAlign.end,
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        // 3
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 40),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Expanded(
-                                child: Text(
-                                  "Asr",
-                                  textAlign: TextAlign.start,
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                              Expanded(
-                                child: Text(
-                                  "6:14",
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                              Expanded(
-                                child: Text(
-                                  "6:35",
-                                  textAlign: TextAlign.end,
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        // 4
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 40),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Expanded(
-                                child: Text(
-                                  "Magrib",
-                                  textAlign: TextAlign.start,
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                              Expanded(
-                                child: Text(
-                                  "6:14",
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                              Expanded(
-                                child: Text(
-                                  "6:35",
-                                  textAlign: TextAlign.end,
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        // 5
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 40),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              Expanded(
-                                child: Text(
-                                  "Isha",
-                                  textAlign: TextAlign.start,
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                              Expanded(
-                                child: Text(
-                                  "6:14",
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                              Expanded(
-                                child: Text(
-                                  "6:35",
-                                  textAlign: TextAlign.end,
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
+                      ),
                     ),
-                  ),
-                ),
+                  )
+                ],
               ),
             ],
           ),
